45b5abf6ae302a02ca7985622809ca6c911211f2
jutudey <aj@eriksen.cz>
2024-11-06 11:55:16 +0100

    Functioning Top 20 lists

--- pages/PnL.py
+++ pages/PnL.py
@@ -19,43 +19,82 @@ st.title("⏳ P&L for Pets and Clients")
 if 'tl' not in st.session_state:
     st.session_state.tl = functions.build_tl()
 
-
 tl = st.session_state.tl
-st.dataframe(tl)
+# st.dataframe(tl)
 
-# Ensure tl_Revenue, tl_Cost, and tl_PetID are numeric
-tl['tl_Revenue'] = pd.to_numeric(tl['tl_Revenue'], errors='coerce')
-tl['tl_Cost'] = pd.to_numeric(tl['tl_Cost'], errors='coerce')
-tl['tl_PetID'] = pd.to_numeric(tl['tl_PetID'], errors='coerce')
+# ----------------------------------------------------
+# defining session states
+# ----------------------------------------------------
 
-# Load pet details from external function
-pet_details_df = functions.get_ezyvet_pet_details()
+ByPet, ByOwner = st.tabs(["P&L by Pets", "P&L by Owners"])
 
-# Ensure Animal Code is numeric
-pet_details_df['Animal Code'] = pd.to_numeric(pet_details_df['Animal Code'], errors='coerce')
+with ByPet:
+    # Group by tl_PetID and calculate sums for tl_Revenue and tl_Cost
+    tl_grouped = tl.groupby('tl_PetID').agg({
+      'tl_PetName': 'first',
+      'tl_CustomerID': 'first',
+      'tl_CustomerName': 'first',
+      'tl_Revenue': 'sum',
+      'tl_Cost': 'sum'
+    }).reset_index()
 
-# Merge pet details into tl DataFrame
-tl = tl.merge(pet_details_df[['Animal Code', 'Animal Name', 'Owner Contact Code', 'Owner Last Name', 'Owner First Name']],
-              how='left',
-              left_on='tl_PetID',
-              right_on='Animal Code')
+    # Add a new column for revenue minus cost
+    tl_grouped['tl_Profit'] = tl_grouped['tl_Revenue'] - tl_grouped['tl_Cost']
 
-# Rename columns to match expected output
-tl.rename(columns={'Animal Name': 'tl_PetName', 'Owner Contact Code': 'tl_CustomerID', 'Owner Last Name': 'tl_CustomerLastName', 'Owner First Name': 'tl_CustomerFirstName'}, inplace=True)
+    # Rename columns
+    tl_grouped.rename(columns={
+        'tl_PetName': 'Pet Name',
+        'tl_PetID': 'Pet eV ID',
+        'tl_CustomerID': 'Customer eV ID',
+        'tl_CustomerName': 'Customer Name',
+        'tl_Revenue': 'Revenue',
+        'tl_Cost': 'Cost',
+        'tl_Profit': 'P&L',
+        'tl_Comment': 'Comment'}, inplace=True)
 
-# Group by tl_PetID and calculate sums for tl_Revenue and tl_Cost
-tl_grouped = tl.groupby('tl_PetID').agg({
-  'tl_PetName': 'first',
-  'tl_CustomerID': 'first',
-  'tl_CustomerLastName': 'first',
-  'tl_CustomerFirstName': 'first',
-  'tl_Revenue': 'sum',
-  'tl_Cost': 'sum'
-}).reset_index()
 
-# Add a new column for revenue minus cost
-tl_grouped['tl_Profit'] = tl_grouped['tl_Revenue'] - tl_grouped['tl_Cost']
+    # Show the top 20 entries with the highest P&L
+    st.subheader("📈   Top 20 most profitable pets")
+    top_20_pl = tl_grouped.nlargest(20, 'P&L')
+    st.dataframe(top_20_pl)
+
+    # Show the top 20 entries with the lowest P&L
+    st.subheader("📉   Top 20 most expensive pets")
+    top_20_lowest_pl = tl_grouped.nsmallest(20, 'P&L')
+    st.dataframe(top_20_lowest_pl)
+
+with ByOwner:
+    # Group by tl_PetID and calculate sums for tl_Revenue and tl_Cost
+    tl_grouped = tl.groupby('tl_CustomerID').agg({
+        'tl_CustomerName': 'first',
+        'tl_Revenue': 'sum',
+        'tl_Cost': 'sum'
+    }).reset_index()
+
+    # Add a new column for revenue minus cost
+    tl_grouped['tl_Profit'] = tl_grouped['tl_Revenue'] - tl_grouped['tl_Cost']
+
+    # Rename columns
+    tl_grouped.rename(columns={
+        'tl_PetName': 'Pet Name',
+        'tl_PetID': 'Pet eV ID',
+        'tl_CustomerID': 'Customer eV ID',
+        'tl_CustomerName': 'Customer Name',
+        'tl_Revenue': 'Revenue',
+        'tl_Cost': 'Cost',
+        'tl_Profit': 'P&L',
+        'tl_Comment': 'Comment'}, inplace=True)
+
+    # Show the top 20 entries with the highest P&L
+    st.subheader("📈  Top 20 most profitable pets")
+    top_20_pl = tl_grouped.nlargest(20, 'P&L')
+    st.dataframe(top_20_pl)
+
+    # Show the top 20 entries with the lowest P&L
+    st.subheader("📉  Top 20 most expensive pets")
+    top_20_lowest_pl = tl_grouped.nsmallest(20, 'P&L')
+    st.dataframe(top_20_lowest_pl)
 
 # Display grouped tl DataFrame
-st.subheader("Grouped tl DataFrame by tl_PetID")
+st.subheader("P&L for all pets")
 st.dataframe(tl_grouped)
