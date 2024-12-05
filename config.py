3d2bbcc41ccd62f4993dfbd31e2c6fe1be6f742b
jutudey <aj@eriksen.cz>
2024-12-05 09:43:16 +0100

    Push RAmona v0.4 - added password protection back in

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 data_folder = 'data'
 
-app_name = "RAmona v0.3"
+app_name = "RAmona v0.4"
 
 required_files_description = [['ezyVet Invoice Lines',
                                    'Invoice Lines Report',
